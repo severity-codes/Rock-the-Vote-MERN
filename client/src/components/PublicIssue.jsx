8909b84c59c589d86ab3a9b74f682f0bc1675c74
Severity Codes <122500980+severity-codes@users.noreply.github.com>
2024-04-24 07:50:25 -0400

    xo

--- client/src/components/PublicIssue.jsx
+++ client/src/components/PublicIssue.jsx
@@ -14,26 +14,21 @@ const PublicIssue = (props) => {
   const formattedTime = moment(createdAt).fromNow();
 
   return (
-    <div className='issue-container'>
-      <div>
-        <div className='user-info'>
-          <div className='profile-pic'>{firstLetter}</div>
-          <div className='name-time'>
-            <div>
-              <h3>{usernameCased}</h3>
-              <p>{formattedTime}</p>
-            </div>
-          </div>
-        </div>
-        <div className='issue-wrapper'>
-          <div className='issue-div'>
-            <h3 className='issue-title'>{title}</h3>
-            <p className='issue-des'>{description}</p>
-            <img className='issue-img' src={imgUrl} alt={title} width={300} />
-          </div>
-        </div>
+    
+      <><div>
+      <div className='user-info'>
+        <div className='profile-pic'>{firstLetter}</div>
+
       </div>
-    </div>
+    </div><div className='issue-wrapper'>
+        <div className='issue-div'>
+          <h3 className='issue-title'>{title}</h3>
+          <p className='issue-des'>{description}</p>
+          <img className='issue-img' src={imgUrl} alt={title} width={300} />
+        </div>
+      </div></>
+     
+ 
   );
 };
 
